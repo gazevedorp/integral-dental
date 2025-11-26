f12398af2d0d218084c88c075a1e972587d3d3de
Your Azevedo <gabrieldma27@gmail.com>
2025-11-26 13:06:57 -0300

    chore: new improvements

--- supabase/idss-storage-policies.sql
+++
@@ -1,4 +0,0 @@
-CREATE POLICY "Allow public uploads" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'idss-documents');
-CREATE POLICY "Allow public updates" ON storage.objects FOR UPDATE TO public USING (bucket_id = 'idss-documents');
-CREATE POLICY "Allow public deletes" ON storage.objects FOR DELETE TO public USING (bucket_id = 'idss-documents');
-CREATE POLICY "Allow public reads" ON storage.objects FOR SELECT TO public USING (bucket_id = 'idss-documents');
