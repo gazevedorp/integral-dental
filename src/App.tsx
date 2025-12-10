18e113cd9bf6197d98c7e722adad69557dfdfd2e
Gabriel Azevedo <gabrieldma27@gmail.com>
2025-12-10 19:15:49 -0300

    chore: remove unused pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Duvidas from './pages/Duvidas';
-import Ouvidoria from './pages/Ouvidoria';
 import CanalEtico from './pages/CanalEtico';
 import Downloads from './pages/Downloads';
 import ParaDentistas from './pages/ParaDentistas';
 import ParaPacientes from './pages/ParaPacientes';
-import PlanosAtendidos from './pages/PlanosAtendidos';
+// import PlanosAtendidos from './pages/PlanosAtendidos';
 import News from './pages/News';
 import NewsArticle from './pages/NewsArticle';
 import PlanoEmpresarial from './pages/PlanoEmpresarial';
@@ -25,12 +24,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
           <Route path="/duvidas" element={<Duvidas />} />
-          <Route path="/ouvidoria" element={<Ouvidoria />} />
           <Route path="/canal-etico" element={<CanalEtico />} />
           <Route path="/downloads" element={<Downloads />} />
           <Route path="/para-dentistas" element={<ParaDentistas />} />
           <Route path="/para-pacientes" element={<ParaPacientes />} />
-          <Route path="/planos-atendidos" element={<PlanosAtendidos />} />
+          {/* <Route path="/planos-atendidos" element={<PlanosAtendidos />} /> */}
           <Route path="/noticias" element={<News />} />
           <Route path="/noticias/:slug" element={<NewsArticle />} />
           <Route path="/plano-empresarial" element={<PlanoEmpresarial />} />
